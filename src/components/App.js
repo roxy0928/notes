21d80a60702e56e76e687a687294079411523e23
roxy0928 <anh.tran@9dots.org>
2019-06-30 19:38:35 -0700

    created Header

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./Header";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Authentication />
+    </div>
+  );
+}
+
+export default App;
